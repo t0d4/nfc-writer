415644de2947220fe9c838c5c916a5bdd99b628c
t0d4 <todatakaaki@gmail.com>
2024-09-22 02:48:59 +0900

    feat: implement UI and main logic

---
+++ main.py
@@ -0,0 +1,85 @@
+from typing import Annotated, Optional
+
+import typer
+from rich.console import Console
+from rich.rule import Rule
+
+import util.nfcutil as nfcutil
+import util.qrutil as qrutil
+
+app = typer.Typer(add_completion=False)
+
+
+@app.command(help="カメラでQRコードをスキャンしてそのURIをNFCタグに書き込む")
+def scanwrite(test_after_write: Annotated[Optional[bool], typer.Argument()] = True):
+    console = Console()
+
+    with qrutil.get_handler() as qr_handler, nfcutil.get_handler() as nfc_handler:
+        while True:
+            with console.status(
+                "[bold cyan] カメラにQRコードをかざしてください...", spinner="line"
+            ):
+                try:
+                    visitor_connpass_uri = qr_handler.try_detect_with_camera(
+                        pattern=r"https://connpass.com/user/.+"
+                    )
+                except qrutil.StopDetection:
+                    console.print(":black_square_for_stop: 処理の中止が要求されました")
+                    return  # quit command
+                else:
+                    console.print(
+                        f":white_check_mark: URIを検出しました: {visitor_connpass_uri}"
+                    )
+
+            with console.status(
+                "[bold cyan] URIを書き込みます。NFCタグをタッチしてください...",
+                spinner="line",
+            ):
+                try:
+                    nfc_handler.write_uri(uri=visitor_connpass_uri)
+                except nfcutil.NDEFError:
+                    console.print(
+                        ":x: [bold red] このNFCタグにはURIレコードを書き込むことが出来ません。QRコードのスキャンに戻ります..."
+                    )
+                    continue
+                else:
+                    console.print(":white_check_mark: URIの書き込みが完了しました")
+
+            if test_after_write:
+                with console.status(
+                    "[bold cyan] URIが正常に書き込まれたかチェックします。NFCタグをタッチしてください...",
+                    spinner_style="bold cyan",
+                ):
+                    if nfc_handler.test_uri(uri=visitor_connpass_uri):
+                        console.print(
+                            ":white_check_mark: URIが正常に書き込まれていることを確認しました"
+                        )
+                    else:
+                        console.print(
+                            ":x: [bold red] このNFCタグには正しいURIレコードが書き込まれていません。QRコードのスキャンに戻ります..."
+                        )
+                        continue
+
+            console.print(Rule(f"URI: {visitor_connpass_uri} の書き込みが完了しました"))
+
+
+@app.command(help="NFCタグに含まれるすべてのレコードを取得して表示する")
+def showrecords():
+    console = Console()
+
+    with nfcutil.get_handler() as nfchandler:
+        with console.status(
+            "[bold cyan] NFCタグをタッチしてください...", spinner="line"
+        ):
+            try:
+                records = nfchandler.get_records()
+            except nfcutil.NDEFError:
+                console.print(
+                    ":x: [bold red] このNFCタグにはNFC Data Exchange Format (NDEF)形式のレコードがありません"
+                )
+            else:
+                console.print(records)
+
+
+if __name__ == "__main__":
+    app()
